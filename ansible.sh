6432ebc2135887023f640226ce67538e041117f1
Your Name <you@example.com>
2020-11-27 04:19:12 +0000

    added .sh

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #create this file in /home/centos called aws_iamuser and input your aws access key id and secret access key. 
+. ~/r1soft*/install-ansible.sh
 . ~/aws_useriam  
 function cleanup {
   if test -f "$1"; then  
@@ -7,5 +8,5 @@ function cleanup {
   fi
 }
 
-cd ~/r1_soft_task/r1soft-agent
+cd ~/r1soft*/
 ansible-playbook -i qa -l all r1_agent.yml -b
---
+++ install-ansible.sh
@@ -0,0 +1,5 @@
+sudo yum install epel-release -y
+sudo yum install ansible -y
+sudo yum install python2-pip -y
+sudo pip install boto
+sudo pip install boto3
